ad84c143c087962bb0c28e2c148f0d601c2e4260
694259015-Moss <>
2026-06-25 16:31:30 +0700

    name.java

---
+++ Name.java
@@ -0,0 +1,7 @@
+void main() {
+    IO.println("Enter your name:");
+    String name = IO.readln();
+    IO.println("Enter your age:");
+    int age = Integer.parseInt(IO.readln());
+    IO.println("Hello, your name is " + name + ", your age is " + age);
+}
