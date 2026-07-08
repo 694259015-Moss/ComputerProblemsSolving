d08d7439377ab13b17c02055802ba934dfbce457
694259015-Moss <694259015@webmail.npru.ac.th>
2026-07-08 16:56:26 +0700

    Added Week5

--- week5/Fuel.java
+++ week5/Fuel.java
@@ -1,1 +1,18 @@
-gi
+void main() {
+    IO.print("Enter your distance: ");
+    int distance = Integer.parseInt(IO.readln());
+
+    IO.print("Enter Fuel Efficiency (lit/km): ");
+    int Effi = Integer.parseInt(IO.readln());
+
+    IO.print("Enter fuel price per litter: ");
+    int price = Integer.parseInt(IO.readln());
+
+    double used = 5.0;
+    double cost = 60.0;
+    int fuel = distance / Effi;
+    double costed = used * price;
+
+    IO.println("Your Fuel is Used " + fuel);
+    IO.println("Your Total Costed " + costed);
+}
